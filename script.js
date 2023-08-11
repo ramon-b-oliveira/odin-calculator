aad666995d38397968be5fd59d3889029b6a341e
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-11 11:37:18 -0400

    create variables for calculator operations

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNum = parseInt(prompt('Type a number'));
+let operator = prompt('Choose an operator');
+let secondNum = parseInt(prompt('Type a number'));
