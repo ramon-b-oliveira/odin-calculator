c7c15d74c3a3e6b0a76625b184e2bb74aac03283
Ramon Oliveira <ramon.braga.o1997@gmail.com>
2023-08-14 12:19:49 -0400

    make calculator work

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+const calculating = document.querySelector(".calculating");
+const current = document.querySelector(".current");
+const numBtns = document.querySelectorAll(".btn")
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+
+for (let i = 0; i < numBtns.length; i++) {
+    numBtns[i].addEventListener("click", btnPressing);
+}
+
+function btnPressing() {
+    if (this.hasAttribute("data-number")) {
+        if (operator === "") {
+            firstNum += this.innerHTML
+            current.innerHTML = firstNum
+        } else {
+            secondNum += this.innerHTML
+            current.innerHTML = secondNum
+        }
+
+    }
+    if (this.hasAttribute("data-operator")) {
+        if (secondNum !== "") {
+            firstNum = operate(firstNum, secondNum, operator)
+        }
+        if (firstNum === "") {
+            return;
+        } else {
+            operator = this.innerHTML
+            calculating.innerHTML = `${firstNum}${this.innerHTML} `
+            secondNum = ""
+        }
+    }
+    if (this.hasAttribute("data-clear")) {
+        firstNum = "";
+        operator = "";
+        secondNum = "";
+        calculating.innerHTML = ""
+        current.innerHTML = "0"
+    }
+    if (this.hasAttribute("data-equals")) {
+        if (operator === "/" && secondNum === "0") {
+            firstNum = "";
+            operator = "";
+            secondNum = "";
+            calculating.innerHTML = ""
+            current.innerHTML = "0"
+            alert("You can't divide by 0")
+        } 
+        if (secondNum === "") {
+            return;
+        } else {
+            let temp = 0
+            calculating.innerHTML = `${firstNum}${operator}${secondNum}=`
+            temp = operate(firstNum, secondNum, operator)
+            current.innerHTML = temp
+            secondNum = ""
+            firstNum = temp
+        }
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -11,24 +74,24 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    let result = a / b
+    if (result !== 0) {
+        return result.toFixed(3)
+    }
+    return result
 }
 
 function operate(firstNum, secondNum, operator) {
     if (operator === "+") {
-        alert(add(firstNum, secondNum));
+        return add(parseFloat(firstNum), parseFloat(secondNum));
     }
     if (operator === "-") {
-        alert(subtract(firstNum, secondNum));
+        return subtract(parseFloat(firstNum), parseFloat(secondNum));
     }
     if (operator === "*") {
-        alert(multiply(firstNum, secondNum));
+        return multiply(parseFloat(firstNum), parseFloat(secondNum));
     }
     if (operator === "/") {
-        alert(divide(firstNum, secondNum));
+        return divide(parseFloat(firstNum), parseFloat(secondNum));
     }
 }
-
-let firstNum = ""
-let operator = ""
-let secondNum = ""
